c9ca2e2830f08d3b4b8d4bb941fda37497ad072e
muhamadmaulana <maulanamuhamad593@gmail.com>
2022-02-21 15:51:07 +0700

    test

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -4,7 +4,7 @@ import Header from '../components/component/Header';
 function Data() {
   return (
   <div>
-      <h1>Ini Adalah Data</h1>
+      <h1>Ini Adalah Data aku</h1>
       <Header/>
   </div>
   );
