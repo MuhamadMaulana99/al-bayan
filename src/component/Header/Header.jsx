766d0bc0867e00beedfab9686b1ecb8297df216d
muhamadmaulana <maulanamuhamad593@gmail.com>
2022-03-16 17:58:09 +0700

    <feat>(common): add tugas 10_intoducing react

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -6,19 +6,15 @@ import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import { pink } from '@mui/material/colors';
 import SvgIcon from '@mui/material/SvgIcon';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 const style = {
   width: '100%',
   maxWidth: 300,
   bgcolor: 'background.paper',
 };
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  );
-}
 export default function Header() {
   const [open, setOpen] = React.useState(true);
 
@@ -28,7 +24,7 @@ export default function Header() {
   return (
     <div>
       <div className='w-full bg-teal-100 h-16 flex justify-start items-center z-30 sticky top-0'>
-          <h1 className='text-current pl-2'>Al-Bayan</h1>
+          <h1 className='text-current pl-2'></h1>
           <Box
             sx={{
                   '& > :not(style)': {
@@ -36,19 +32,28 @@ export default function Header() {
                   },
             }}
           >
-            <HomeIcon color="primary" fontSize="large" />
+
           </Box>
       </div>
         <List sx={style} component="nav" aria-label="mailbox folders">
-          <ListItem button>
+          <ListItem className='flex items-center justify-center' button>
+            <Box>
+              <HomeOutlinedIcon/>
+            </Box>
             <ListItemText primary="Home" />
           </ListItem>
           <Divider />
           <ListItem button divider>
-            <ListItemText primary="Contac" />
+            <Box>
+              <ContactsOutlinedIcon/>
+            </Box>
+              <ListItemText primary="Contac" />
           </ListItem>
           <ListItem button>
-            <ListItemText primary="About" />
+            <Box>
+              <AccountCircleOutlinedIcon/>
+            </Box>
+              <ListItemText primary="About" />
           </ListItem>
           <Divider />
     </List>
